63b2af866caa82795f3ce975853312101c251d3c
Krammm08 <Krammm08@github.com>
2026-03-20 15:08:19 +0800

    Fixed AppointmentDAO

--- src/com/app/dao/AppointmentDAO.java
+++ src/com/app/dao/AppointmentDAO.java
@@ -1,6 +1,14 @@
 package com.app.dao;
 
+import com.app.model.Appointment;
+import java.util.List;
+
 public interface AppointmentDAO {
-    
+
+    boolean addAppointment(Appointment appointment);
+    List<Appointment> getAllAppointments();
+    Appointment getAppointmentById(int appointmentId);
+    boolean updateAppointment(Appointment appointment);
+    boolean deleteAppointment(int appointmentId);
+
 }
- 
