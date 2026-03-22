138f538114c3ea372848b36d0a72c0771124bd5e
Krammm08 <Krammm08@github.com>
2026-03-22 13:13:17 +0800

    Updaated MedicineService

--- src/com/app/service/MedicineService.java
+++ src/com/app/service/MedicineService.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.app.service;
 
-/**
- *
- * @author Mathet
- */
-public class MedicineService {
-    
+import com.app.model.Medicine;
+import java.util.List;
+
+public interface MedicineService {
+    boolean addmedicine(Medicine medicine);
+    List<Medicine> getAllMedicines();
+    Medicine getMedicineById(int id);
+    boolean updateMedicine(Medicine medicine);
+    boolean deleteMedicine(int id);
 }
